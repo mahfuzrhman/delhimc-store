cd4232d044060e7ad9dd45b1a4322f79da001da0
mahfuzrhman <116345526+mahfuzrhman@users.noreply.github.com>
2025-07-23 18:48:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".product button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    const form = btn.nextElementSibling;
-    form.style.display = form.style.display === "block" ? "none" : "block";
-  });
-});
